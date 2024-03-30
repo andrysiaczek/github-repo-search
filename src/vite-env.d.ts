812ef7b68ff9f8cb0d94ff8aa682e04304c0d9fc
andrysiaczek <agata.and.11@gmail.com>
2024-03-30 01:07:23 +0100

    configure environment variables for api integration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GITHUB_API_URI: string;
+  readonly VITE_GITHUB_PERSONAL_ACCESS_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
